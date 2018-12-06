7c56e282a733a12061f7d8f41ab2a6d9fd9f51e9
wei1113 <45652937+wei1113@users.noreply.github.com>
2018-12-06 16:57:30 +0800

    Update ex10.ino

--- ex10.ino
+++ ex10.ino
@@ -1,1 +1,47 @@
+int t;
+int h;
 
+char _lwifi_ssid[] = "Wei";
+char _lwifi_pass[] = "1234567890";
+
+MCSDevice mcs("DCJMF2B9", "ajkFrS8rdrmEVZic");
+MCSDisplayInteger temp("temp");
+MCSDisplayInteger humid("humid");
+
+DHT __dht2(4, DHT11);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  mcs.addChannel(temp);
+  mcs.addChannel(humid);
+  Serial.println("Wi-Fi");
+  while (WiFi.begin(_lwifi_ssid, _lwifi_pass) != WL_CONNECTED) { delay(1000); }
+  Serial.println("Wi-Fi");
+  while(!mcs.connected()) { mcs.connect(); }
+  Serial.println("MCS");
+  Serial.begin(9600);
+  __dht2.begin();
+}
+
+void loop()
+{
+  while (!mcs.connected()) {
+    mcs.connect();
+    if (mcs.connected()) { Serial.println("MCS 已重新連線"); }
+  }
+  mcs.process(100);
+
+  t = __dht2.readTemperature();
+  h = __dht2.readHumidity();
+  temp.set(t);
+  humid.set(h);
+
+  Serial.print("攝氏溫度 : ");
+  Serial.println(t);
+  Serial.print("相對溼度 :");
+  Serial.println(h);
+
+  delay(1000);
+}
